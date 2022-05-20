5d212470ae103d514f5237e4a5ffcd18c3d2f9e0
jinlee9270 <93522658+jinlee9270@users.noreply.github.com>
2022-05-20 22:58:26 +0900

    Update 공유기 설치.py

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -15,19 +15,14 @@ while left <= right:
     dis = (left + right) // 2
 
     for i in range(0, n):
-        # print(home[i],start_point,dis)
         if home[i] - start_point >= dis:
             start_point = home[i]
             wifi = wifi + 1
-            # print("start_point",start_point,"dis=",dis,"wifi=",wifi)
-
+            
     if wifi >= c:
         left = dis + 1
-        # print(MX_dis,dis)
         if MX_dis < dis:
             MX_dis = dis
-            # print(MX_dis)
     else:
         right = dis - 1
-    # print(dis,wifi)
 print(MX_dis)
