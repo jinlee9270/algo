54e19b70cb83a7c24a515372724d6d654ba82706
jinlee9270 <93522658+jinlee9270@users.noreply.github.com>
2022-06-07 22:35:28 +0900

    Update 셀프 넘버.py

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -4,17 +4,18 @@ Hund = 0
 Thou = 0
 Temp = 0
 num = [0] * 10001
+
 for i in range(1, 10001):
+    
     Thou = i // 1000
     Temp = i % 1000
     Hund = Temp // 100
     Temp = i % 100
     Ten = Temp // 10
     One = Temp % 10
+    
     if i+Thou+Hund+Ten+One <= 10000:
         num[i+Thou+Hund+Ten+One] = 1
-
-# print(num)
 
 for i in range(1, 10001):
     if num[i] == 0:
