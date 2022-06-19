be9d583f3429d26e6f638ba6a2161017c8560ec7
jinlee9270 <93522658+jinlee9270@users.noreply.github.com>
2022-06-19 22:13:40 +0900

    Update 리모컨.py

--- 백준/Gold/1107. 리모컨/리모컨.py
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -2,7 +2,6 @@ import sys
 target_channel = int(sys.stdin.readline())
 n = int(sys.stdin.readline())
 broken = set(map(int, sys.stdin.readline().split()))
-# print(broken)
 
 answer = abs(100 - target_channel)
 for i in range(0, 999900):
@@ -23,6 +22,5 @@ for i in range(0, 999900):
 
         if available:
             answer = min(answer, pressed_num + abs(i - target_channel))
-    # print(i, answer, pressed_num, target_channel)
 
 print(answer)
