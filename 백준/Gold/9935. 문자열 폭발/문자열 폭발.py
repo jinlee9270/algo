5ef014de2f98f3503f460dc766d1174d8ebecdcc
jinlee9270 <93522658+jinlee9270@users.noreply.github.com>
2022-06-26 23:09:40 +0900

    [Gold IV] Title: 문자열 폭발, Time: 248 ms, Memory: 215428 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -0,0 +1,28 @@
+strings = list(input())
+bomb_string = list(input())
+
+stack = []
+for string in strings:
+    # print(stack)
+    is_bomb = False
+    if len(stack) < len(bomb_string) - 1:
+        stack.append(string)
+
+    else:
+        # print(string,bomb_string[-1])
+        if string == bomb_string[-1]:
+            stack.append(string)
+            # print(stack[len(stack) - len(bomb_string):])
+            if stack[len(stack) - len(bomb_string):] == bomb_string:
+                temp = 0
+                while temp < len(bomb_string):
+                    stack.pop()
+                    temp = temp + 1
+        else:
+            stack.append(string)
+
+
+if len(stack) > 0:
+    print("".join(stack))
+else:
+    print("FRULA")
